c423b37200f801fb16bd3f03492d642615c4dc30
yidi <yidi.hu@epfl.ch>
2019-10-20 19:52:34 +0800

    1. removed dropdown from tab3 and link the graph to slider. 2. add in interval component

--- Mainv5.2.py
+++ Mainv5.2.py
@@ -42,6 +42,10 @@ def load_data():
     user_list = sorted(trade_table.User.unique())
     return trade_table, portfolio_list, user_list
 
+def save_file(df, user):
+    df = df[df.user==user]
+    df.to_csv(join(csv_path, user+'.csv'), index=False)
+
 trade_table, portfolio_list, user_list = load_data()
 ratio_list = ['Sharpe Ratio', 'Hit Ratio', 'Sortino Ratio']
 
@@ -71,10 +75,16 @@ def tab2_build_table(trade_table, user):
 ## function for table
 # input: df
 # output: df
-def tab3_get_data(trade_table, user):
+def tab3_get_data(trade_table, user, start, end):
     ### filter to get user data
     trade_table['Size/Notional'] = trade_table['Size/Notional'].astype('int')
     trade_table = trade_table[trade_table.User==user]
+    
+    ### 
+    if start:
+        trade_table = trade_table[trade_table.Timestamp>=start]
+    if end:
+        trade_table = trade_table[trade_table.Timestamp<=end]
     
     #create an empty df to hold data
     groupby_product = pd.DataFrame(columns=['Portfolio','Size/Notional'])
@@ -96,7 +106,7 @@ def tab3_get_data(trade_table, user):
 # ouput: dash_table.DataTable
 def tab3_build_table(trade_table, user):
     #trade_table = pd.DataFrame.from_dict(trade_table_store)
-    data_df = tab3_get_data(trade_table, user)
+    data_df = tab3_get_data(trade_table, user, None, None)
     return dash_table.DataTable(
         id = 'tab3 product table',
         columns = [{"name": i, "id": i} for i in data_df.columns],
@@ -317,91 +327,72 @@ def init_tab_2():
         ])
 
 def init_tab_3():
+    selector_date = html.Div([
+        html.Div([
+            html.H6('Select Date:')], 
+            style = {'margin-left': 10, 'margin-right': 20}),
+        dcc.DatePickerRange(
+            id = 'tab3_date_range',
+            display_format='Y-M-D',
+            end_date = dt.now().date(),
+            start_date = min(trade_table['Timestamp'])
+        )],
+        className = 'row',
+        style = {'display': 'flex', 'flex-direction': 'row', 'align-items': 'center', "margin-top": 30}
+    )
+    
+    selector_portfolio = html.Div([
+        html.H6('Select Portfolio'),
+        dcc.Dropdown(id = 'tab3 portfolio')],
+        style = {'padding':8})
+    
+    ratio_container = html.Div(
+        id="info-container",
+        children = tab3_build_ratio(ratio_list),
+        className="four columns",
+        style = {'padding': 10})
+    
+    selector_section = html.Div([
+        selector_date, selector_portfolio], 
+        className = "four columns")
+    
+    
+    pnl_header = html.Div(
+        className = "row chart-top-bar",
+        children = [
+            html.Div(
+                className="inline-block chart-title",
+                children = "PnL Performance")])
+    
+    pnl_graph = dcc.Graph(
+        id='tab3 daily pnl', 
+        figure = go.Figure(
+            layout = go.Layout({"paper_bgcolor": "rgba(0,0,0,0)",
+                                "plot_bgcolor": "rgba(0,0,0,0)",
+                                "font": {"color": "lightgrey"},
+                                "margin": {'t': 30}}))) 
+    
+    pnl_section = html.Div([
+        pnl_header,pnl_graph], 
+        className = "eight columns",
+        style = {'padding': 10, 'width': 800})
+    
+    table = html.Div(
+        id='tab3_table',
+        children = tab3_build_table(trade_table,user_list[0]),
+        className = 'eight columns',
+        style = {'height': 400, 'padding': 10, 'width': 800})
+    
     return dcc.Tab(label='Individual Analysis', className = 'custom-tab', selected_className="custom-tab--selected", 
                    children = [
                        html.Div([  
-                           html.Div([
-                               html.Div([
-                                   ###Dropdown to select user
-#                                   html.Div([
-#                                       html.H6('Select User'),
-#                                       dcc.Dropdown(id = 'tab3 user',
-#                                                    options = [{'label': 'Trader '+i, 'value': i} for i in user_list],
-#                                                    value=user_list[0])],
-#                                       style = {'padding':8},
-#                                   ),
-                                   
-                                   html.Div([
-                                       html.Div([html.H6('Select Date:')], style = {'margin-left': 10, 'margin-right': 20}),
-                                       dcc.DatePickerRange(
-                                            id = 'tab3_date_range',
-                                            display_format='Y-M-D',
-                                            end_date = dt.now().date(),
-                                            start_date = min(trade_table['Timestamp'])
-                                       )],
-                                       className = 'row',
-                                       style = {'display': 'flex', 'flex-direction': 'row', 'align-items': 'center', "margin-top": 30}
-                                   ),
-                                   html.Div([
-                                       html.H6('Select Portfolio'),
-                                       dcc.Dropdown(id = 'tab3 portfolio')],
-                                       style = {'padding':8}),
-                               ], 
-                                   className = "four columns"), 
-                               
-                               html.Div([
-                                   html.Div(
-                                       className = "row chart-top-bar",
-                                       children = [
-                                           
-                                           html.Div(
-                                               className="inline-block chart-title",
-                                               children = "PnL Performance",
-                                           ),
-                                           html.Div(
-                                               className = "graph-top-right inline-block",
-                                               children = [
-                                                   html.Div([
-                                                       dcc.Dropdown(id = 'tab3 time unit',
-                                                                    options=[{'label': 'Daily', 'value': 'day'},
-                                                                             {'label': 'Weekly', 'value': 'week'},
-                                                                             {'label': 'Monthly', 'value': 'month'}],
-                                                                    value='day')],
-                                                       style = {'width': 300}),
-                                               ]
-                                           )
-                                       ]
-                                   ),
-                                   dcc.Graph(id='tab3 daily pnl', 
-                                             figure = go.Figure(
-                                                                layout = go.Layout({"paper_bgcolor": "rgba(0,0,0,0)",
-                                                                                    "plot_bgcolor": "rgba(0,0,0,0)",
-                                                                                    "font": {"color": "lightgrey"},
-                                                                                    "margin": {'t': 30}})
-                                                      )
-                                            )               
-                               ], 
-                                   className = "eight columns",
-                                   style = {'padding': 10, 'width': 800})
-                           ], 
-                               className = "twelve columns",
-                               style = {'margin-left': 30, 'margin-top': 10, 'margin-bottom': 30}),  
+                           html.Div([selector_section, pnl_section], 
+                                    className = "twelve columns",
+                                    style = {'margin-left': 30, 'margin-top': 10, 'margin-bottom': 30}),  
                            
-                           html.Div([
-                               html.Div(id="info-container",
-                                        children = tab3_build_ratio(ratio_list),
-                                        className="four columns",
-                                        style = {'padding': 10}
-                                       ),
-                               html.Div(id='tab3_table',
-                                        children = tab3_build_table(trade_table,user_list[0]),
-                                        className = 'eight columns',
-                                        style = {'height': 400, 'padding': 10, 'width': 800}
-                                       )
-                           ],
-                               className = "twelve columns",
-                               style = {'margin-left': 30, 'margin-top': 10, 'margin-bottom': 30}
-                           )
+                           html.Div([ratio_container, table],
+                                    className = "twelve columns",
+                                    style = {'margin-left': 30, 'margin-top': 10, 'margin-bottom': 30})
                        ],
                            style = {'margin-left': 30, 'margin-right': 30, 'margin-top': 20, 'margin-bottom': 30}
                        )
@@ -559,9 +550,21 @@ app.layout = html.Div(
                      init_tab_4()
         ]),
         
-        dcc.Store(id='trade_table_store', data = trade_table.to_dict('records'))
+        dcc.Store(id='trade_table_store', data = trade_table.to_dict('records')),
+        dcc.Interval(
+            id='interval-component',
+            interval=1*1000, # in milliseconds
+            n_intervals=0
+        )
     ]
 )
+
+@app.callback(Output('trade_table_store', 'data'),
+              [Input('interval-component', 'n_intervals')])
+def update_data_source(n):
+    trade_table, portfolio_list, user_list = load_data()
+    return trade_table.to_dict('records')
+
 
 ## tab 1 display portfolios
 @app.callback(Output('tab1_total_portfolio', 'children'),
@@ -603,15 +606,14 @@ def update_tab1_pnl(user):
                         name = portfolio) for portfolio in portfolios                    
                     ]}    
 
-# tab 2 update table
+#tab2 add transaction
 @app.callback(
     [Output('confirm', 'displayed'),
-     Output('trade_table_store', 'data'),
-     Output('tab2_trade_table', 'data'),
      Output('user_login', 'options'),
      Output('user_login', 'value')],
     [Input('add', 'submit_n_clicks'),
-     Input('tab2_trade_table', "sort_by")],
+     Input('tab2_trade_table', "sort_by"),
+     Input('trade_table_store', 'data')],
     [State('user_login', 'value'),
      State('portfolio', 'value'),
      State('product', 'value'),
@@ -625,9 +627,11 @@ def update_tab1_pnl(user):
      State('strategy', 'value'),
      State('timestamp', 'date'),
      State('user', 'value')])
-def update_table(submit_n_clicks, sort_by, login, portfolio, type, product, direction, price, size, tenor, risk, timeframe, strategy, timestamp, user):
+def update_table(submit_n_clicks, sort_by, data_dict, login, portfolio, type, product, direction, price, size, tenor, risk, timeframe, strategy, timestamp, user):
+    trade_table = pd.DataFrame.from_dict(data_dict)
     trade_user = trade_table[trade_table.User == login]
     options = [{'label': user, 'value': user}for user in user_list]
+    
     if len(sort_by):
         trade_df = trade_user.sort_values(
         [col['column_id'] for col in sort_by],
@@ -639,11 +643,13 @@ def update_table(submit_n_clicks, sort_by, login, portfolio, type, product, dire
         )
     else:
         trade_df = trade_user
+        
     if submit_n_clicks:
         inputs = [portfolio, type, product, direction, price, size, tenor, risk, timeframe, strategy, timestamp, user]
         for input in inputs:
             if input == None:
-               return True, trade_table.to_dict('records'), trade_df.to_dict('records'), options, login
+                return True,  options, login
+            
         index = len(trade_table)
         trade_table.loc[index, 'Portfolio'] = portfolio
         trade_table.loc[index, 'Type of Trade'] = type
@@ -659,12 +665,16 @@ def update_table(submit_n_clicks, sort_by, login, portfolio, type, product, dire
         trade_table.loc[index, 'User'] = user
         trade_table[trade_table.User == user].to_csv(join(csv_path, user + '.csv'), index = False)
         trade_user = trade_table[trade_table.User == user]
+        
         if (user not in user_list):
             user_list.append(user)
             user_list.sort()
             options = [{'label': user, 'value': user}for user in user_list]
-        return False, trade_table.to_dict('records'), trade_user.to_dict('records'), options, user 
-    return False, trade_table.to_dict('records'), trade_df.to_dict('records'), options, login
+        
+        save_file(trade_table, user)
+        return False, options, user 
+    
+    return False, options, login
 
 ### tab 2 clear input
 #@app.callback(
@@ -708,60 +718,46 @@ def update_portfolio(user, data_dict):
     pfl_list = sorted(data.Portfolio.unique())
     return [{'label': 'Portfolio '+i, 'value': i} for i in pfl_list]
 
-###tab3 update table by user
-@app.callback(Output('tab3 product table', 'data'),
-              [Input('user_login', 'value'),
-               Input('trade_table_store', 'data'),
-               Input('tab3 product table', 'sort_by')])
-def update_tab3_table(user, data_dict, sort_by):
+###tab3 table
+@app.callback(
+    Output('tab3 product table', 'data'),
+    [Input('tab3_date_range', 'start_date'),
+     Input('tab3_date_range', 'end_date'),
+     Input('trade_table_store', 'data'),
+     Input('user_login', 'value'),
+     Input('tab3 product table','sort_by')
+    ])
+def update_table(start_date, end_date, data_dict, user, sort_by):
     data = pd.DataFrame.from_dict(data_dict)
-    df = tab3_get_data(data,user)
+    df = tab3_get_data(data,user, start_date, end_date)
+    
     if len(sort_by):
         trade_df = df.sort_values(
-        [col['column_id'] for col in sort_by],
-        ascending=[
-            col['direction'] == 'asc'
-            for col in sort_by
-        ],
-        inplace=False
-        )
-    else:
-        trade_df = df
+            [col['column_id'] for col in sort_by],
+            ascending=[col['direction'] == 'asc' for col in sort_by],
+            inplace=False)
+    else: trade_df = df
+    
     return trade_df.to_dict('records')
 
 ###tab3 update graph
 @app.callback(Output('tab3 daily pnl', 'figure'),
-              [Input('tab3 time unit', 'value'),
-               Input('tab3 portfolio', 'value'),
-               Input('user_login', 'value')],
-              [State('trade_table_store', 'data')])
-def update_tab3_daily(time,portfolio,user, trade_table_store):
-    trade_table = pd.DataFrame.from_dict(trade_table_store)
+              [Input('tab3_date_range', 'start_date'),
+               Input('tab3_date_range', 'end_date'),
+               Input('trade_table_store', 'data'),
+               Input('user_login', 'value'),
+               Input('tab3 portfolio', 'value')])
+def update_tab3_daily(start, end, data_dict, user, portfolio):
+    trade_table = pd.DataFrame.from_dict(data_dict)
     temp_df = trade_table.loc[(trade_table['Portfolio']==portfolio) 
                               & (trade_table['User'] == user)]
     
-    if time == 'day':
-        temp_df = temp_df[['Timestamp', 'Price']].groupby('Timestamp').sum()
-        temp_df = temp_df.reset_index(level=['Timestamp'])
-        temp_df = temp_df.sort_values('Timestamp')
-                
-    elif time in ['week', 'month']:
-        ### first calculate daily pnl then add up days of same time frame
-        temp_df = temp_df[['Timeframe', 'Price']].groupby('Timeframe').sum()
-        temp_df = temp_df.reset_index(level=['Timeframe'])
-        temp_df['Timestamp'] = temp_df['Timeframe']
-        temp_df = temp_df.sort_values('Timestamp')
-        
-        if time == 'month':
-            ### add up weeks 4 by 4
-            week_num = temp_df.Timestamp.tolist()
-            price = temp_df.Price.tolist()
-            
-            monthly_pnl = []
-            
-            for i in range(0,len(week_num),4):
-                monthly_pnl.append(['WW'+str(week_num[i])+'~WW'+str(week_num[min((i+3),len(week_num)-1)]), sum(price[i:i+4])])
-            temp_df = pd.DataFrame(monthly_pnl,columns=['Timestamp','Price'])
+    if start: temp_df = temp_df[temp_df.Timestamp>=start]
+    if end: temp_df = temp_df[temp_df.Timestamp<=end]
+    
+    temp_df = temp_df[['Timestamp', 'Price']].groupby('Timestamp').sum()
+    temp_df = temp_df.reset_index(level=['Timestamp'])
+    temp_df = temp_df.sort_values('Timestamp')
 
     X = temp_df['Timestamp']
     Y = temp_df['Price']
@@ -775,7 +771,6 @@ def update_tab3_daily(time,portfolio,user, trade_table_store):
                       },
             'data': [go.Scatter(x = X, y = Y, mode = 'lines+markers')]
            }  
-
 
 
 # tab 4 graph
