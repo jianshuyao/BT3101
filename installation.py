ac2af80820baf70fbaa75ceb1348bf57927a8ab7
jianshuyao <jianshuyao@u.nus.edu>
2019-11-04 21:23:06 +0800

    install&upgrade

--- installation.py
+++
@@ -1,5 +0,0 @@
-pip install pandas
-pip install numpy
-pip install dash==1.4.1 
-pip install dash-daq==0.2.1  
-pip install plotly --upgrade
---
+++ installation.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+pip install dash 
+pip install dash-core-components 
+pip install dash-html-components 
+pip install numpy 
+pip install pandas 
+pip install plotly 
+pip install xlrd 
+pip install Click 
+pip install Flask 
+pip install Flask-Compress 
+pip install future 
+pip install itsdangerous 
+pip install Jinja2 
+pip install MarkupSafe 
+pip install python-dateutil 
+pip install pytz 
+pip install retrying 
+pip install six 
+pip install Werkzeug 
